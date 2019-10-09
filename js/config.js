25cf8b7235ca538ac27bf1528cf193e7de8d9438
VictorRaim <v.raimbourg@gmail.com>
2019-10-09 11:07:23 +0200

    adding Cognito Service for authentication

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_pEl18zGbX', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3b7len3k9p3fsl3sosnpoeo481', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
